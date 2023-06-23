faeb49a1849bb8641e631664d2a9a0e71b96fa1d
jaewoo Kim <wodndhksk@gmail.com>
2023-06-23 15:06:23 +0900

    트랜잭션 전파 활용1 - 예제 프로젝트 시작

---
+++ src/main/java/hello/springtx/propagation/Member.java
@@ -0,0 +1,28 @@
+package hello.springtx.propagation;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+
+    public Member() {
+
+    }
+
+    public Member(String username) {
+        this.username = username;
+    }
+}
