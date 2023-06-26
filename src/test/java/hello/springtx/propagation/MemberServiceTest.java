f1c6f839982060a6e4a72e9e2d92c74622c9d73b
jaewoo Kim <wodndhksk@gmail.com>
2023-06-26 09:14:09 +0900

    트랜잭션 전파 활용2 - 커밋, 롤백
    - 트랜잭션 각각 동작

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
@@ -35,6 +36,27 @@ class MemberServiceTest {
         //then : 모든 데이터가 정상 저장된다.
         assertTrue(memberRepository.find(username).isPresent());
         assertTrue(logRepository.find(username).isPresent());
+
+    }
+
+ /**
+     *  memberService           @Transactional:OFF
+     *  memberRepository        @Transactional:ON
+     *  logRepository           @Transactional:ON  Exception
+     */
+    @Test
+    void outerTxOff_fail() {
+        //given
+        String username = "로그예외_outerTxOff_fail";
+
+        //when
+        assertThatThrownBy(() -> memberService.joinV1(username))
+                .isInstanceOf(RuntimeException.class);
+//        memberService.joinV1(username);
+
+        //then : 모든 데이터가 정상 저장된다.
+        assertTrue(memberRepository.find(username).isPresent()); // 정상 커밋 결과
+        assertTrue(logRepository.find(username).isEmpty()); // 예외로 인한 롤백
 
     }
 
